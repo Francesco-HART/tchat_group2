4d4ab0a0c27393ac7332bb958dc419578146f7f1
francesco <fha@m2mfactory.fr>
2020-11-12 11:04:33 +0100

    start server strct

--- chatServeur/db/demo_mongo.js
+++ chatServeur/db/demo_mongo.js
@@ -1,22 +1,16 @@
-require('dotenv').config()
-const { MongoClient } = require('mongodb')
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/DaMovies'
-
-const client = new MongoClient(process.env.MONGO_URI)
+const MONGO_URI =
+  "mongodb://uq6uyt020a3iwte4amvc:EP2YZPueMLVHWrnAdcuu@bnzzp8d394kena7-mongodb.services.clever-cloud.com:27017/bnzzp8d394kena7";
+const client = new MongoClient(MONGO_URI);
 
 async function main() {
-    await client.connect() // on se connecte au serveur mongo
-    const db = client.db('DaMovies') // use DaMovies
+  await client.connect(); // on se connecte au serveur mongo
+  const db = client.db("bnzzp8d394kena7"); // use DaMovies
 
-    // const coll_list = await db.collections()
-    // console.log({ colls: coll_list[0].s })
-
-    const movies = db.collection('movies')
-    // ne pas oublier le .toArray() quand on .find une collection
-    const artistOrScientist = await db.collection('users').findOne({ occupation: { $in: ['artist', 'scientist'] } })
-    console.log(artistOrScientist)
-
+  // const coll_list = await db.collections()
+  // console.log({ colls: coll_list[0].s })
 }
-
-main()
+module.exports = client;
+main();
---
+++ chatServeur/routes/message.js
@@ -0,0 +1,7 @@
+//const { requireAuth, requireAdmin } = require("../middlewares");
+const Message = require("../controllers/message");
+
+module.exports = (app) => {
+  //app.get("/api/message", Auth.signIn);
+  //app.put("/api/message", Massage.sendMessage);
+};
