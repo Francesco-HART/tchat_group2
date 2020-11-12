57197eeda3853e8d78373ddfd6ef899a622818dd
Guillaume <guillaume.pinheiro@edu.itescia>
2020-11-12 15:10:26 +0100

    fix

--- chatServeur/router.js
+++ chatServeur/router.js
@@ -1,6 +1,5 @@
 module.exports = (app) => {
   require("./routes/auth")(app);
   require("./routes/rooms")(app);
-  require("./routes/public_message")(app);
-  require("./routes/private_message")(app);
+  require("./routes/message")(app);
 };
