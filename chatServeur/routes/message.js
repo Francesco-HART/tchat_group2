01b3ab33d9cf3c0b53c8da9d52b00f9df4b3fec6
francesco <fha@m2mfactory.fr>
2020-11-12 14:55:59 +0100

    maj

--- chatServeur/routes/message.js
+++ chatServeur/routes/message.js
@@ -3,12 +3,9 @@ const MessagePrivate = require("../controllers/private_messages");
 const MessagePublic = require("../controllers/public_messages");
 module.exports = (app) => {
   //app.get("/api/message", Auth.signIn);
-  app.post("/api/message", MessagePrivate.createMessage);
-  app.get("/api/getusersentmessages", MessagePrivate.getUserSentMessages);
-  app.get(
-    "/api/getuserreceivedmessages",
-    MessagePrivate.getUserReceivedMessages
-  );
+  app.post("/api/message/private", MessagePrivate.createMessage);
+  app.get("/api/message/user/sent", MessagePrivate.getUserSentMessages);
+  app.get("/api/message/user/receive", MessagePrivate.getUserReceivedMessages);
   app.post("/api/message/public", MessagePublic.createRoomMessage);
   app.get("/api/message/public", MessagePublic.getRoomsMessages);
 };
