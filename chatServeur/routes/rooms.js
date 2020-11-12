3a31f8c64bfd2eefd1a97d358bf567a39d741f61
Francesco <55229758+Francesco-HART@users.noreply.github.com>
2020-11-13 00:08:21 +0100

    Revert "Message"

--- chatServeur/routes/rooms.js
+++
@@ -1,9 +0,0 @@
-//const { requireAuth, requireAdmin } = require("../middlewares");
-const Rooms = require("../controllers/rooms");
-
-module.exports = (app) => {
-  app.post("/api/rooms", Rooms.createRoom);
-  app.get("/api/rooms", Rooms.getRoomWithMessages);
-  app.get("/api/all-rooms", Rooms.getRoom);
-
-};
--- chatServeur/sevices/socket.js
+++
@@ -1,8 +0,0 @@
-const io = require("socket.io")();
-
-//join socket groups
-io.on("connection", (socket) => {
-  console.log("I am connect");
-});
-
-module.exports = io;
