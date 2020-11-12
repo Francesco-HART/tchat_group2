3cd182cb5b803607d0538e008f00d5f9d57ef492
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2020-11-12 14:23:54 +0100

    try find(); can't have all, async pb

--- chatServeur/db/Mongo.Class.js
+++ chatServeur/db/Mongo.Class.js
@@ -11,6 +11,7 @@ class MongoClass{
         this.db = this.dbConnect()
     }
 
+    // pourrait surement etre ameliorer, pour ne pas avoir a faire un await sur chaque getDb()
     async dbConnect(dbName = "bnzzp8d394kena7"){
         const client = new MongoClient('mongodb://uq6uyt020a3iwte4amvc:EP2YZPueMLVHWrnAdcuu@bnzzp8d394kena7-mongodb.services.clever-cloud.com:27017/bnzzp8d394kena7')
         await client.connect() //  connexion au serveur mongo
@@ -21,18 +22,31 @@ class MongoClass{
         return this.db;
     }
 
+    //region user collection
     async getCollectionUser(){
         const db = await this.getDb();
         return db.collection("users");
     }
 
-    async findUserByName(data) {
+    async findUserByPseudo(pseudo) {
         const collection = await this.getCollectionUser();
-        return await collection.findOne({pseudo: data.pseudo})
+        return await collection.findOne({pseudo: pseudo})
     }
 
+    /*async findSeveralUserByPseudo(pseudos) {
+        const collection = await this.getCollectionUser();
+        const result = await collection.find({pseudo: {$in: pseudos}});
+        const res = await result.toArray(async function (err, res) {
+            if (err) throw err;
+            console.log(res);
+            return res;
+        });
+        console.log(res)
+        return res;
+    }*/
+
     async insertUser(data){
-        const dbData = this.findUserByName(data)
+        const dbData = this.findUserByPseudo(data.pseudo)
         if (dbData===null || dbData===undefined){
             const hashedPassword = passwordHash.generate(data.password);
             const collection = await this.getCollectionUser();
@@ -45,10 +59,22 @@ class MongoClass{
     }
 
     async isUserAuth(data){
-        const dbData = this.findUserByName(data)
+        const dbData = this.findUserByPseudo(data.pseudo)
         const hashedPassword = dbData.password;
         return passwordHash.verify(data.password, hashedPassword);
     }
+    //endregion
+
+    async getCollectionMessage(){
+        const db = await this.getDb();
+        return db.collection("messages");
+    }
+
+    async findMessageByUserName(data) {
+        const collection = await this.getCollectionMessage();
+        return await collection.find({pseudo: data.pseudo})
+    }
+
 }
 
 exports.MongoClass = MongoClass
--- chatServeur/db/demo_mongo.js
+++ chatServeur/db/demo_mongo.js
@@ -1,15 +1,10 @@
 
 const db = require('../db/mongo')
 async function main() {
-  /*await client.connect(); // on se connecte au serveur mongo
-  const db = client.db("bnzzp8d394kena7"); // use DaMovies
-  */
-  console.log( await db.users.findUserByName({pseudo:"new user"}));
+  console.log( await db.users.findUserByPseudo("new user"));
   console.log(await db.users.isUserAuth({pseudo: "new user", password:"password"}))
-  //console.log(await db.users.insertUSer({pseudo: "new test", password:"password"}))
+  //console.log( await db.users.findSeveralUserByPseudo(["new user"]));
 
-  // const coll_list = await db.collections()
-  // console.log({ colls: coll_list[0].s })
 }
 
 main();
