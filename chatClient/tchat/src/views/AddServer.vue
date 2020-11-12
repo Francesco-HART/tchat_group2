c73538a5bb2d2ffdbcc798b7fd86eacc6af2fd23
Antony Correia <antony.correia@grtgaz.com>
2020-11-12 12:36:25 +0100

    add route and some page

---
+++ chatClient/tchat/src/views/AddServer.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-container style="height: 100%">
+    <b-row style="height: 100%">
+      <b-col style="display: flex ; justify-content : center; align-items: center">
+        <md-card style="width: 300px; height: 200px">
+
+          <md-card-content>
+            <md-field>
+              <label>Nom serveur</label>
+              <md-input v-model="server"></md-input>
+            </md-field>
+          </md-card-content>
+
+          <md-card-actions style="justify-content: center">
+            <md-button class="md-raised" v-on:click="onSend">Envoyer</md-button>
+          </md-card-actions>
+
+        </md-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'AddServer',
+  data: () => ({
+    server: ''
+  }),
+  methods: {
+    onSend: function () {
+      this.$store.commit('listServers', this.server)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ chatClient/tchat/src/views/Auth.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-container style="height: 100%">
+    <b-row style="height: 100%">
+      <b-col style="display: flex ; justify-content : center; align-items: center">
+        <md-card style="width: 300px; height: 300px">
+
+            <md-card-content>
+              <md-field>
+                <label>Login</label>
+                <md-input v-model="login"></md-input>
+              </md-field>
+            </md-card-content>
+
+            <md-card-content>
+              <md-field>
+                <label>Password</label>
+                <md-input type="password" v-model="password"></md-input>
+              </md-field>
+            </md-card-content>
+
+            <md-card-actions style="justify-content: center">
+              <md-button class="md-raised" v-on:click="onLogin">Envoyer</md-button>
+            </md-card-actions>
+
+        </md-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Auth',
+  data: () => ({
+    login: '',
+    password: ''
+  }),
+  methods: {
+    onLogin: function () {
+      console.log(this.login)
+      console.log(this.password)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
