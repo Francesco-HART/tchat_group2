728d1d52b2985f6793415e93e7286f430bcf9a2d
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2020-11-13 14:12:19 +0100

    end jwt

--- chatServeur/routes/message.js
+++ chatServeur/routes/message.js
@@ -1,11 +1,13 @@
 //const { requireAuth, requireAdmin } = require("../middlewares");
 const MessagePrivate = require("../controllers/private_messages");
 const MessagePublic = require("../controllers/public_messages");
+const { requireAuth } = require("../middlewares");
+
 module.exports = (app) => {
   //app.get("/api/message", Auth.signIn);
-  app.post("/api/message/private", MessagePrivate.createMessage);
-  app.get("/api/message/user/sent", MessagePrivate.getUserSentMessages);
-  app.get("/api/message/user/receive", MessagePrivate.getUserReceivedMessages);
-  app.post("/api/message/public", MessagePublic.sendPublicMessage);
-  app.get("/api/message/public", MessagePublic.getPublicMessage);
+  app.post("/api/message/private", requireAuth,  MessagePrivate.createMessage);
+  app.get("/api/message/user/sent", requireAuth, MessagePrivate.getUserSentMessages);
+  app.get("/api/message/user/receive", requireAuth, MessagePrivate.getUserReceivedMessages);
+  app.post("/api/message/public", requireAuth, MessagePublic.sendPublicMessage);
+  app.get("/api/message/public", requireAuth, MessagePublic.getPublicMessage);
 };
